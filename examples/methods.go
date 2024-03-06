bb1c61912c1fcee2ee2c17779f94757e6c5f1b4c
Pratik Sah <pratik.sah@razorpay.com>
2024-03-07 02:44:02 +0530

    Added methods to non-struct types

--- examples/methods.go
+++ examples/methods.go
@@ -6,12 +6,20 @@ type Hello struct {
 	name string
 }
 
+type Age int
+
+func (a Age) PrintAge() {
+	fmt.Printf("User age is %v\n", a)
+}
+
 func (h Hello) methodExample() {
-	fmt.Println(h.name)
+	fmt.Printf(h.name)
 }
 
 func TestMethodInGo() {
 	d := Hello{"Pratik"}
-
 	d.methodExample()
+
+	userAge := Age(28)
+	userAge.PrintAge()
 }
