9656786aaaa1dbb6c9b0aa5cd0fdcfca57e23885
Pratik Sah <pratik.sah@razorpay.com>
2024-03-07 00:57:33 +0530

    Working with range, loops, slice

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-	println("Hello")
+	dummyArr := []int{}
+
+	fmt.Println(dummyArr)
+
+	dummyArr = append(dummyArr, 1)
+	fmt.Println(dummyArr)
+
+	for i := 0; i < 5; i++ {
+		dummyArr = append(dummyArr, i)
+	}
+
+	fmt.Println(dummyArr)
+
+	for _, num := range dummyArr {
+		fmt.Println(num)
+	}
 }
