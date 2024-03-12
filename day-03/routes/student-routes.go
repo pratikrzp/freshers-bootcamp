8b450982d0209a1c1c214bc2f4d3a857107971d3
Pratik Sah <pratik.sah@razorpay.com>
2024-03-12 12:54:45 +0530

    fixed route group

--- day-03/routes/student-routes.go
+++ day-03/routes/student-routes.go
@@ -2,15 +2,20 @@ package route
 
 import (
 	"day03/controllers"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
 )
 
 func StudentRoutes(router *gin.Engine) {
-	r1 := router.Group("/user")
-	r1.GET("/", controllers.ReadAllStudent)
-	r1.GET("/:id", controllers.ReadStudent)
-	r1.POST("/", controllers.CreateStudent)
-	r1.PUT("/:id", controllers.UpdateStudent)
-	r1.DELETE("/:id", controllers.DeleteStudent)
+	r1 := router.Group("/student", func(ctx *gin.Context) {
+		fmt.Println("New request received with student group")
+	})
+	{
+		r1.GET("/", controllers.ReadAllStudent)
+		r1.GET("/:id", controllers.ReadStudent)
+		r1.POST("/", controllers.CreateStudent)
+		r1.PUT("/:id", controllers.UpdateStudent)
+		r1.DELETE("/:id", controllers.DeleteStudent)
+	}
 }
